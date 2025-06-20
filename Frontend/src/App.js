56a87938a298a4185429158c73ba2b821a3edad9
Imran980486 <ImranKhan@dal.ca>
2025-06-19 22:24:08 -0300

    Update App.js

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>EventSpark Calculator dev1</h1>
+        <h1>EventSpark Calculator dev</h1>
         <p>A simple calculator with Spring Boot backend and React frontend</p>
       </header>
       <main>
