82ce15360c6c10a3bf18567582f04fe9dec98c19
Imran980486 <ImranKhan@dal.ca>
2025-06-20 00:54:37 -0300

    Update Calculator.js

--- Frontend/src/components/Calculator.js
+++ Frontend/src/components/Calculator.js
@@ -12,7 +12,7 @@ const Calculator = () => {
 
   // Get API URL from environment variable or use default
   const API_BASE_URL =
-    process.env.REACT_APP_BACKEND_URL || "https://eventspark-ufix.onrender.com";
+    process.env.REACT_APP_BACKEND_URL || "http://localhost:8080";
 
   const operations = [
     { symbol: "+", name: "add" },
