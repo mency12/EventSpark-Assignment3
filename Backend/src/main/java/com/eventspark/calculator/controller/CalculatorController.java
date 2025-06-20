3694f2e3fa8e006717801d46bf9af1d3353fd397
Imran980486 <ImranKhan@dal.ca>
2025-06-19 23:13:54 -0300

    Update CalculatorController.java

--- Backend/src/main/java/com/eventspark/calculator/controller/CalculatorController.java
+++ Backend/src/main/java/com/eventspark/calculator/controller/CalculatorController.java
@@ -2,7 +2,6 @@ package com.eventspark.calculator.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,11 +14,6 @@ import com.eventspark.calculator.service.CalculatorService;
 
 @RestController
 @RequestMapping("/api/calculator")
-@CrossOrigin(origins = {
-    "http://localhost:3000",
-    "https://*.vercel.app",
-    "https://*.onrender.com"
-})
 public class CalculatorController {
 
     @Autowired
