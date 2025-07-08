50d6fa6d426758d8e8f2134c90df1342499dbb29
shray <sh290173@dal.ca>
2025-07-08 04:41:01 -0300

    CRUD fix 7

--- Backend/services/eventServices.js
+++ Backend/services/eventServices.js
@@ -1,4 +1,4 @@
-const Event = require('../models/Event');
+const Event = require('../models/Events');
 
 /* ─────────────  CREATE  ─────────────────────────────────────── */
 exports.createEvent = async (req, res, next) => {
